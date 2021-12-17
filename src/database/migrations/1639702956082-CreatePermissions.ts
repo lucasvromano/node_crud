1a9935d0bc73588cd1985bb93c6a282272e0629b
Lucas <lucas.romano.acc>
2021-12-16 22:13:41 -0300

    alterar user para name

---
+++ src/database/migrations/1639702956082-CreatePermissions.ts
@@ -0,0 +1,30 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreatePermissions1639702956082 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "permissions",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("permissions");
+  }
+}
---
+++ src/database/migrations/1639703379254-CreateUsers.ts
@@ -0,0 +1,48 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsers1639703379254 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "password",
+            type: "varchar",
+          },
+          {
+            name: "permission_id",
+            type: "uuid",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "fk_users_category",
+            columnNames: ["permission_id"],
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
+}
